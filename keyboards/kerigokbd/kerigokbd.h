9c5e124223e700782f944e21c2fb64cfe8dc7d98
Ryotaro Onuki <kerikun11+github@gmail.com>
2024-12-12 22:26:46 +0900

    fix dynamic keycode issue

--- keyboards/kerigokbd/kerigokbd.h
+++ keyboards/kerigokbd/kerigokbd.h
@@ -2,10 +2,9 @@
 
 #include <quantum.h>
 #include <keymap_japanese.h>
+#include <dynamic_keymap.h>
 
-// #define KEY_NUM LT(1,JP_MHEN)
 #define KEY_NUM MO(1)
-// #define KEY_FUN LT(2,JP_HENK)
 #define KEY_FUN MO(2)
 #define KEY_ALT LALT_T(JP_MHEN)
 #define KEY_WIN RWIN_T(JP_HENK)
