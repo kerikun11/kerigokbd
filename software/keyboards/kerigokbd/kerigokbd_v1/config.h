01617c5d75e5be15c56b42f5256dee9cf36a87b7
Ryotaro Onuki <kerikun11+github@gmail.com>
2025-02-02 14:39:11 +0900

    delete pointing device

--- software/keyboards/kerigokbd/kerigokbd_v1/config.h
+++ software/keyboards/kerigokbd/kerigokbd_v1/config.h
@@ -6,28 +6,3 @@
 
 /* USB */
 #define USB_VBUS_PIN GP26
-
-/* I2C */
-#define I2C_DRIVER I2CD0 // see https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf#page=14
-#define I2C1_SDA_PIN GP28
-#define I2C1_SCL_PIN GP29
-
-/* Pointing Device (https://docs.qmk.fm/features/pointing_device#common-configuration) */
-#define SPLIT_POINTING_ENABLE
-#define POINTING_DEVICE_RIGHT
-// #define POINTING_DEVICE_GESTURES_SCROLL_ENABLE // circular scroll
-
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 4
-#define AUTO_MOUSE_TIME 1000
-// #define AUTO_MOUSE_DELAY 200
-
-#define TRACKPAD_DRAGSCROLL_REVERSE_X
-
-/* Cirque Trackpad (https://docs.qmk.fm/features/pointing_device#cirque-trackpad) */
-#define CIRQUE_PINNACLE_DIAMETER_MM 35
-#define CIRQUE_PINNACLE_TAP_ENABLE
-// #define CIRQUE_PINNACLE_ATTENUATION EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_1X //< most sensitive
-#define CIRQUE_PINNACLE_ATTENUATION EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_2X
-// #define CIRQUE_PINNACLE_ATTENUATION EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_3X
-// #define CIRQUE_PINNACLE_ATTENUATION EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_4X //< least sensitive
--- software/keyboards/kerigokbd/kerigokbd_v1/keymaps/default/keymap.c
+++ software/keyboards/kerigokbd/kerigokbd_v1/keymaps/default/keymap.c
@@ -33,10 +33,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                _______, _______, _______, _______, /**/ _______, _______, _______, _______
   ),
   [4] = LAYOUT_split_6_7_7_4(
-    _______, QK_KB_0, QK_KB_2, MS_BTN3, XXXXXXX, XXXXXXX,          /**/          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, QK_KB_1, QK_KB_3, QK_KB_6, QK_KB_4, XXXXXXX, XXXXXXX, /**/ MS_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ MS_BTN1, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                               XXXXXXX, MS_BTN2, MS_BTN1, XXXXXXX, /**/ MS_BTN3, XXXXXXX, XXXXXXX, XXXXXXX
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          /**/          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+                               _______, _______, _______, _______, /**/ _______, _______, _______, _______
   ),
   [5] = LAYOUT_split_6_7_7_4(
     XXXXXXX, RGB_SAI, RGB_HUI, RGB_VAI, RGB_SPI, RGB_MOD,          /**/          RGB_MOD, RGB_SPI, RGB_VAI, RGB_HUI, RGB_SAI, XXXXXXX,
@@ -48,76 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #endif
 // clang-format on
 
-#ifdef POINTING_DEVICE_ENABLE
-#    if defined(POINTING_DEVICE_AUTO_MOUSE_ENABLE)
-void pointing_device_init_user(void) {
-    set_auto_mouse_enable(true); // always required before the auto mouse feature will work
+#if 0
+void keyboard_post_init_user(void) {
+    debug_enable   = true;
+    debug_matrix   = true;
+    debug_keyboard = true;
+    debug_mouse    = true;
 }
-bool is_mouse_record_kb(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QK_KB_0 ... QK_KB_7:
-            return true;
-    }
-    return is_mouse_record_user(keycode, record);
-}
-#    endif // POINTING_DEVICE_AUTO_MOUSE_ENABLE
-
-enum custom_keycodes {
-    DRAG_SCROLL = QK_KB_6,
-};
-
-bool set_scrolling = false;
-
-// Modify these values to adjust the scrolling speed
-#    define SCROLL_DIVISOR_H 16.0
-#    define SCROLL_DIVISOR_V 16.0
-
-// Variables to store accumulated scroll values
-float scroll_accumulated_h = 0;
-float scroll_accumulated_v = 0;
-
-// Function to handle mouse reports and perform drag scrolling
-report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    // Check if drag scrolling is active
-    if (set_scrolling) {
-        // Calculate and accumulate scroll values based on mouse movement and divisors
-        scroll_accumulated_h += (float)mouse_report.x / SCROLL_DIVISOR_H;
-        scroll_accumulated_v += (float)mouse_report.y / SCROLL_DIVISOR_V;
-
-        // Assign integer parts of accumulated scroll values to the mouse report
-        mouse_report.h = (int8_t)scroll_accumulated_h;
-        mouse_report.v = (int8_t)scroll_accumulated_v;
-
-        // Update accumulated scroll values by subtracting the integer parts
-        scroll_accumulated_h -= (int8_t)scroll_accumulated_h;
-        scroll_accumulated_v -= (int8_t)scroll_accumulated_v;
-
-        // Clear the X and Y values of the mouse report
-        mouse_report.x = 0;
-        mouse_report.y = 0;
-    }
-    return mouse_report;
-}
-
-// Function to handle key events and enable/disable drag scrolling
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case DRAG_SCROLL:
-            // Toggle set_scrolling when DRAG_SCROLL key is pressed or released
-            set_scrolling = record->event.pressed;
-            break;
-        default:
-            break;
-    }
-    return true;
-}
-
-// Function to handle layer changes and disable drag scrolling when not in AUTO_MOUSE_DEFAULT_LAYER
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // Disable set_scrolling if the current layer is not the AUTO_MOUSE_DEFAULT_LAYER
-    if (get_highest_layer(state) != AUTO_MOUSE_DEFAULT_LAYER) {
-        set_scrolling = false;
-    }
-    return state;
-}
-#endif // POINTING_DEVICE_ENABLE
+#endif
