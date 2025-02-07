16d8bc08d008a79fc228d73c72f4d9023ac1fda5
Ryotaro Onuki <kerikun11+github@gmail.com>
2025-02-07 23:43:01 +0900

    update keymap

--- software/keyboards/kerigokbd/kerigokbd_v1/keymaps/default/keymap.c
+++ software/keyboards/kerigokbd/kerigokbd_v1/keymaps/default/keymap.c
@@ -9,7 +9,7 @@
 #ifdef LAYOUT_split_6_7_7_4
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_6_7_7_4(
-     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          /**/             KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          /**/             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
     KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   MO(5), /**/   MO(5),    KC_H,    KC_J,    KC_K,    KC_L, JP_MINS,  KC_ENT,
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KG_PSCR, /**/ KG_PSC2,    KC_N,    KC_M, JP_COMM,  JP_DOT, JP_SLSH,  KC_DEL,
                                 KG_WIN,  KG_ALT,  KG_NUM,  KC_ESC, /**/  KC_SPC,  KG_FUN,  KG_WIN,  KG_ALT
@@ -18,13 +18,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, JP_EXLM, JP_DQUO, JP_HASH,  JP_DLR, JP_PERC,          /**/          JP_AMPR, JP_QUOT, JP_ASTR, JP_CIRC, JP_TILD, _______,
     _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_VOLU, /**/ KC_BRIU,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
     _______,  JP_GRV,   JP_AT, JP_SCLN, JP_COLN, JP_UNDS, KC_VOLD, /**/ KC_BRID, JP_PIPE, JP_PLUS,  JP_EQL,  JP_DOT, JP_BSLS, _______,
-                               _______, _______, _______, _______, /**/ _______, _______, _______, _______
+                               _______, KC_LWIN, _______, _______, /**/ _______, _______, KC_RALT, _______
   ),
   [2] = LAYOUT_split_6_7_7_4(
     _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,          /**/            KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
     _______, JP_LPRN, JP_LCBR, JP_RCBR, JP_LBRC, JP_RBRC,  KC_F11, /**/  KC_F12, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, JP_RPRN, _______,
     _______, MS_BTN2, MS_WHLL, MS_WHLU, MS_WHLD, MS_WHLR, MS_BTN3, /**/ MS_BTN3, MS_LEFT, MS_DOWN,   MS_UP, MS_RGHT, MS_BTN1, _______,
-                               _______, _______, _______, _______, /**/ _______, _______, _______, _______
+                               _______, KC_LWIN, _______, _______, /**/ _______, _______, KC_RALT, _______
   ),
   [3] = LAYOUT_split_6_7_7_4(
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          /**/          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
