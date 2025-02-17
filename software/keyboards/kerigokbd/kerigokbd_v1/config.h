252bdf970827a799bff979050212b3c2452611b8
Ryotaro Onuki <kerikun11+github@gmail.com>
2025-02-17 14:22:02 +0900

    update pin assign to KERIgoKBD v1.1

--- software/keyboards/kerigokbd/kerigokbd_v1/config.h
+++ software/keyboards/kerigokbd/kerigokbd_v1/config.h
@@ -5,4 +5,9 @@
 #pragma once
 
 /* USB */
-#define USB_VBUS_PIN GP26
+#define USB_VBUS_PIN GP28
+
+/* I2C */
+// #define I2C_DRIVER I2CD0 // see https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf#page=13
+// #define I2C1_SDA_PIN GP26
+// #define I2C1_SCL_PIN GP27
