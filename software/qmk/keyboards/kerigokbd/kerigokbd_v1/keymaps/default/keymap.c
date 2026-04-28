369b413892d198d37d28dc5a37d322d40e2b7fc7
Ryotaro Onuki <kerikun11+github@gmail.com>
2026-04-28 11:57:10 +0900

    update keymap

--- software/qmk/keyboards/kerigokbd/kerigokbd_v1/keymaps/default/keymap.c
+++ software/qmk/keyboards/kerigokbd/kerigokbd_v1/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,          /**/          KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______,
     _______, JP_LPRN, JP_LCBR, JP_RCBR, JP_LBRC ,JP_RBRC,  KC_F11, /**/ MS_BTN3, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, JP_RPRN, _______,
     KC_LSFT, MS_BTN2, MS_WHLL, MS_WHLU, MS_WHLD, MS_WHLR,  KC_F12, /**/ MS_BTN2, MS_LEFT, MS_DOWN, MS_UP  , MS_RGHT, MS_BTN1, _______,
-                               JP_ZKHK, KC_LWIN, _______, _______, /**/ _______, _______, KC_RALT, KC_RCTL
+                               _______, _______, _______, _______, /**/ _______, _______, _______, _______
   ),
   [KGL_ESC] = LAYOUT_split_6_7_7_4( /* Tenkey and Left Hand Device */
     _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,          /**/          KC_P7  , KC_P8  , KC_P9  , KC_PMNS, KC_PENT, _______,
