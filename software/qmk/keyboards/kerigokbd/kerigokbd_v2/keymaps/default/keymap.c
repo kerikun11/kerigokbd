a4d5f00894f033e4a1b8413237ac284263f255f9
Ryotaro Onuki <kerikun11+github@gmail.com>
2026-05-03 17:20:00 +0900

    update keymap

--- software/qmk/keyboards/kerigokbd/kerigokbd_v2/keymaps/default/keymap.c
+++ software/qmk/keyboards/kerigokbd/kerigokbd_v2/keymaps/default/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,          /**/          KC_P7  , KC_P8  , KC_P9  , KC_PMNS, KC_PENT, _______,
     _______, A(KC_1), A(KC_2), A(KC_3), A(KC_4), A(KC_5),  KC_F11, /**/ KC_PSLS, KC_P4  , KC_P5  , KC_P6  , KC_PPLS, KC_UP  , _______,
     _______, MS_BTN1, MS_BTN3, MS_BTN2, KC_DEL , KC_BSPC,  KC_F12, /**/ KC_PAST, KC_P1  , KC_P2  , KC_P3  , KC_LEFT, KC_DOWN, KC_RGHT,
-                               XXXXXXX, XXXXXXX,  KC_NUM, KG_MAIN, /**/ KC_PDOT, KC_P0  , XXXXXXX, XXXXXXX
+                               KC_SLEP, XXXXXXX,  KC_NUM, KG_MAIN, /**/ KC_PDOT, KC_P0  , XXXXXXX, XXXXXXX
   ),
   [KGL_TEMP] = LAYOUT_split_6_7_7_4( /* Tentative Layer */
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          /**/          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
