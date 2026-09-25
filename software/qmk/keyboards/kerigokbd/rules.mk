97bdfb4fd68c353679a7e92b100eeec8d2bb10a7
Ryotaro Onuki <kerikun11+github@gmail.com>
2026-09-25 15:35:00 +0900

    fix key debounce

--- software/qmk/keyboards/kerigokbd/rules.mk
+++ software/qmk/keyboards/kerigokbd/rules.mk
@@ -2,3 +2,6 @@
 
 # RGB Matrix
 RGB_MATRIX_CUSTOM_USER = yes # rgb_matrix_user.inc
+
+# Debounce: per-key eager (global defer reorders near-simultaneous presses by matrix position)
+DEBOUNCE_TYPE = sym_eager_pk
