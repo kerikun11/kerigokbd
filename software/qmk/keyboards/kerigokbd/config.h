2bc91f063c0ad04c5a9931467146b83f4b55dd43
Ryotaro Onuki <kerikun11+github@gmail.com>
2026-09-25 16:52:12 +0900

    enable HOLD_ON_OTHER_KEY_PRESS

--- software/qmk/keyboards/kerigokbd/config.h
+++ software/qmk/keyboards/kerigokbd/config.h
@@ -11,3 +11,6 @@
 
 /* RGB Matrix */
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CUSTOM_keyfunc // c.f. rgb_matrix_user.inc
+
+/* Tap-Hold (https://docs.qmk.fm/tap_hold#hold-on-other-key-press) */
+#define HOLD_ON_OTHER_KEY_PRESS
